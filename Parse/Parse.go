4cfad303d45da6af7bf77df4aa88141168f9be15
Андрей Владиславов <and.vladislavov@gmail.com>
2019-10-21 17:17:51 +0300

    team comps [2]

--- Parse/Parse.go
+++ Parse/Parse.go
@@ -134,9 +134,7 @@ func getTeam(item *html.Node) (res map[string]string) {
 				ch := getChildren(item.NextSibling.NextSibling.NextSibling.NextSibling)
 				for _, c := range ch {
 					if isElem(c, "a") {
-						res[c.FirstChild.Data] = getAttr(c, "href")
-						//fmt.Println(c)
-						//res = &res2
+						res[c.FirstChild.Data] = "rusfencing.ru" + getAttr(c, "href")
 					}
 				}
 			} else {
