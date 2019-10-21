36663e5884cb60604061203e57206cd2c239c9bf
Андрей Владиславов <and.vladislavov@gmail.com>
2019-10-21 18:30:01 +0300

    final links fix

--- main.go
+++ main.go
@@ -251,12 +251,12 @@ func getResultByLink(link string, categ string) []string {
 		all = append(all, toSend)
 		toSend = ""
 		for _, v := range res[len(res)/3 : len(res)*2/3] {
-			toSend += fmt.Sprintf("%s. <a href=\"rusfencing.ru%s\">%s</a>\n", v.Place, v.Link, v.Name)
+			toSend += fmt.Sprintf("%s. <a href=\"%s\">%s</a>\n", v.Place, v.Link, v.Name)
 		}
 		all = append(all, toSend)
 		toSend = ""
 		for _, v := range res[len(res)*2/3:] {
-			toSend += fmt.Sprintf("%s. <a href=\"rusfencing.ru%s\">%s</a>\n", v.Place, v.Link, v.Name)
+			toSend += fmt.Sprintf("%s. <a href=\"%s\">%s</a>\n", v.Place, v.Link, v.Name)
 		}
 		all = append(all, toSend)
 		return all
